0fe27d0bc6474df9cb90760e2e35a085476b6907
Marek Pechal <mpechal@phys.ethz.ch>
2024-09-22 10:08:48 +0200

    Function for generation of simulated measurements

--- src/quantum_optics/lvovsky.py
+++ src/quantum_optics/lvovsky.py
@@ -8,7 +8,15 @@ Marek Pechal, 2021-2024
 """
 
 import numpy as np
-from quantum_optics.measurements import quadrature_measurement_operator
+import pickle
+from quantum_optics.measurements import (
+    quadrature_measurement_operator,
+    draw_from_pdf,
+    )
+from quantum_optics.core import (
+    expectation,
+    fock_wavefunction,
+    )
 
 def R_op(rho, theta_arr, x_arr, eta=None):
     """R operator defined by Eq. (2) in Lvovsky 2004.
@@ -20,6 +28,9 @@ def R_op(rho, theta_arr, x_arr, eta=None):
         eta (float or None, optional): If not None, the projective measurement
             is preceded by a loss channel with transmission eta.
             Defaults to None.
+
+    Returns:
+        array: The R operator.
     """
     assert rho.shape[0] == rho.shape[1], "density matrix is not square"
     dim = rho.shape[0]
@@ -33,14 +44,60 @@ def R_op(rho, theta_arr, x_arr, eta=None):
 def iteration_step(rho, theta_arr, x_arr, eta=None):
     """Iteration step (Eq. (5)) of the algorithm from Lvovsky 2004.
 
-    rho (array): Density matrix.
-    theta_arr (array): Array of quadrature angle values.
-    x_arr (array): Array of quadrature measurement outcomes.
-    eta (float or None, optional): If not None, the projective measurement
-        is preceded by a loss channel with transmission eta.
-        Defaults to None.
+    Args:
+        rho (array): Density matrix.
+        theta_arr (array): Array of quadrature angle values.
+        x_arr (array): Array of quadrature measurement outcomes.
+        eta (float or None, optional): If not None, the projective measurement
+            is preceded by a loss channel with transmission eta.
+            Defaults to None.
+
+    Returns:
+        array: Updated density matrix.
     """
     R = R_op(rho, theta_arr, x_arr, eta=eta)
     res = R.dot(rho).dot(R)
     res = res/np.trace(res)
     return res
+
+def generate_data(state, num_quadratures, num_reps,
+        x_range, n_subdiv,
+        seed=None, eta=None):
+    """Make example data for Lvovsky algo.
+
+    Args:
+        filename (str): Name of the pickle file to save to.
+        state (array): State vector or density matrix of the state.
+        num_quadratures (int): Number of quadratures to measure.
+        num_reps (int): Number of measurements per quadrature.
+        x_range (tuple): Finite range (x1, x2) from which to draw.
+        n_subdiv (int): Number of intervals into which to subdivide the range.
+        seed (int or None, optional): Seed for the rng. Defaults to None.
+        eta (float or None, optional): Quantum efficiency. If None,
+            eta = 1. Defaults to None.
+
+    Returns:
+        tuple: (theta_arr, x_arr)
+            theta_arr is an array of quadrature angles.
+            x_arr is an array of quadrature values.
+    """
+
+    dim = state.shape[0]
+    is_pure = (len(state.shape)==1)
+    theta_rng = np.linspace(0, np.pi, num_quadratures+1)[:-1]
+    theta_arr = np.array([theta
+        for theta in theta_rng
+        for _ in range(num_reps)])
+    x_arr = []
+    for idx, theta in enumerate(theta_rng):
+        if is_pure:
+            pdf = lambda x: abs(sum([a*fock_wavefunction(n, theta, x)
+                for n, a in enumerate(state)]))**2
+        else:
+            pdf = lambda x: expectation(
+                quadrature_measurement_operator(dim, theta, x, eta=eta),
+                state).real
+        samples = draw_from_pdf(pdf, num_reps, x_range, n_subdiv,
+            seed=(seed+idx) if seed is not None else None)
+        x_arr = np.concatenate((x_arr, samples))
+    return (theta_arr, x_arr)
--- tests/test_lvovsky.py
+++ tests/test_lvovsky.py
@@ -1,38 +1,73 @@
 from quantum_optics.lvovsky import (
     iteration_step,
+    generate_data,
+    )
+from quantum_optics.core import (
+    fock_psi,
+    psi_to_rho,
     )
 import scipy.integrate
 import scipy.interpolate
 import matplotlib.pyplot as plt
 import numpy as np
 
-def _generate_random_numbers(pdf, N, xMax=8.0, seed=None):
-    rng = np.random.default_rng(seed=seed)
-    xRng = np.linspace(-xMax, xMax, 2001)
-    pRng = np.array([pdf(x) for x in xRng])
-    pRng = pRng/sum(pRng)
-    F = scipy.interpolate.interp1d(np.cumsum(pRng), xRng)
-    return np.array([F(rng.uniform()) for _ in range(N)])
-
-def test_reconstruction():
-    theta_rng = np.linspace(0, np.pi/2, 9)
-    reps = 50
+def test_reconstruction_pure():
+    num_quadratures = 8
+    num_reps = 100
     dim = 5
     seed = 0
-    theta_arr = np.array([theta for theta in theta_rng for _ in range(reps)])
-    x_arr = _generate_random_numbers(
-        lambda x: 4*x**2*np.exp(-2*x**2)/np.sqrt(np.pi/2),
-        len(theta_arr)*reps,
-        seed=seed
-        )
+    psi = fock_psi(dim, 1)
+    theta_arr, x_arr = generate_data(psi, num_quadratures, num_reps,
+        (-8.0, 8.0), 2000,
+        seed=seed)
     rho = np.eye(dim).astype(complex)
-    n_iters = 50
+    n_iters = 15
     for _ in range(n_iters):
         rho = iteration_step(rho, theta_arr, x_arr)
     assert (
         abs(np.trace(rho)-1)<1e-6 and
-        abs(rho[1, 1]-1)<0.05), \
+        abs(rho[1, 1]-1)<0.1), \
+        "reconstruction check failed"
+
+def test_reconstruction_mixed():
+    num_quadratures = 8
+    num_reps = 100
+    dim = 5
+    seed = 0
+    psi = fock_psi(dim, 1)
+    rho = psi_to_rho(psi)
+    theta_arr, x_arr = generate_data(rho, num_quadratures, num_reps,
+        (-8.0, 8.0), 2000,
+        seed=seed)
+    rho = np.eye(dim).astype(complex)
+    n_iters = 15
+    for _ in range(n_iters):
+        rho = iteration_step(rho, theta_arr, x_arr)
+    assert (
+        abs(np.trace(rho)-1)<1e-6 and
+        abs(rho[1, 1]-1)<0.1), \
+        "reconstruction check failed"
+
+def test_reconstruction_pure_with_loss():
+    num_quadratures = 16
+    num_reps = 1000
+    dim = 5
+    seed = 0
+    eta = 0.75
+    psi = fock_psi(dim, 1)
+    theta_arr, x_arr = generate_data(psi, num_quadratures, num_reps,
+        (-8.0, 8.0), 2000,
+        seed=seed, eta=eta)
+    rho = np.eye(dim).astype(complex)
+    n_iters = 25
+    for _ in range(n_iters):
+        rho = iteration_step(rho, theta_arr, x_arr, eta=eta)
+    assert (
+        abs(np.trace(rho)-1)<1e-6 and
+        abs(rho[1, 1]-1)<0.1), \
         "reconstruction check failed"
 
 if __name__ == "__main__":
-    test_reconstruction()
+    test_reconstruction_pure()
+    test_reconstruction_mixed()
+    test_reconstruction_pure_with_loss()
