1c52ffe570d6614d14b51ce288457f895f22e49a
Marek Pechal <marek.pechal@gmail.com>
2024-11-30 10:21:29 +0100

    Calculation of emission couplings.

--- src/quantum_optics/kiilerich.py
+++ src/quantum_optics/kiilerich.py
@@ -2,71 +2,71 @@
 
 Based on Kiilerich 2020 (arXiv:2003.04573v1 [quant-ph]).
 Implementation with an arbitrary number of input and output resonators.
-
-1. A number of cascaded systems
-(we do not distinguish between the input and output resonators and the
-quantum system for now)
-
-   |
-   v   a0(t)
-   |
-  [ ]--<>-- [system 1] A1
-   |
-   v   a1(t)
-   |
-  [ ]--<>-- [system 2] A2
-   |
-  ...
-   |
-  [ ]--<>-- [system m] Am
-   |
-   v   am(t)
-   |
-  [ ]--<>-- [system m+1] A{m+1}
-   |
-  ...
-
-Input-output relations:
-a1(t) = a0(t) + g*1(t) A1(t)
-a2(t) = a1(t) + g*2(t) A2(t)
-...
-
-System operator evolution:
-Ak'(t)
-  = ... [unitary evolution] - |gk(t)|^2 Ak(t) / 2
-    - gk(t) a{k-1}(t)
-  = ... [unitary evolution] - |gk(t)|^2 Ak(t) / 2
-    - gk(t) (g*{k-1}(t) A{k-1}(t) + g*{k-2}(t) A{k-2}(t) + ... + a0(t))
-
-Re-write in matrix form (M is a matrix, G a vector):
-
-A'(t) = ... [unitary evolution] + M(t) A(t) - G(t) a0(t)
-where
-M(t) = [
-  [-|g1(t)|^2/2, 0, 0, ...],
-  [-g2(t) g*1(t), -|g2(t)|^2/2, 0, 0, ...],
-  [-g3(t) g*1(t), -g3(t) g*2(t), -|g3(t)|^2/2, 0, 0, ...],
-  ...
-  ]
-and
-G(t) = [g1(t), g2(t), ...]
-
-Find form of master equation which results in the Heisenberg equations above:
-
-rho'(t) = -i[H0(t), rho(t)] [unitary evolution]
-  -i[Hc(t), rho(t)]
-  -D_X(t)[rho(t)]
-
-where
-X(t) = g*1(t) A1(t) + g*2(t) A2(t) - ...
-Hc(t) = -i sum_{i>j} (gi(t) g*j(t) Adi Aj - H.c.) / 2
 """
 
 import qutip as qt
 import numpy as np
+import scipy
+import matplotlib.pyplot as plt
 
 def solve_cascaded_system(dims, Hs, gs, *args, **kwargs):
-    """
+    """Solve evolution of a number of cascaded systems.
+
+       |
+       v   a0(t)
+       |
+      [ ]--<>-- [system 1] A1
+       |
+       v   a1(t)
+       |
+      [ ]--<>-- [system 2] A2
+       |
+      ...
+       |
+      [ ]--<>-- [system m] Am
+       |
+       v   am(t)
+       |
+      [ ]--<>-- [system m+1] A{m+1}
+       |
+      ...
+
+    Input-output relations:
+    a1(t) = a0(t) + g*1(t) A1(t)
+    a2(t) = a1(t) + g*2(t) A2(t)
+    ...
+
+    System operator evolution:
+    Ak'(t)
+      = ... [unitary evolution] - |gk(t)|^2 Ak(t) / 2
+        - gk(t) a{k-1}(t)
+      = ... [unitary evolution] - |gk(t)|^2 Ak(t) / 2
+        - gk(t) (g*{k-1}(t) A{k-1}(t) + g*{k-2}(t) A{k-2}(t) + ... + a0(t))
+
+    Re-write in matrix form (M is a matrix, G a vector):
+
+    A'(t) = ... [unitary evolution] + M(t) A(t) - G(t) a0(t)
+    where
+    M(t) = [
+      [-|g1(t)|^2/2, 0, 0, ...],
+      [-g2(t) g*1(t), -|g2(t)|^2/2, 0, 0, ...],
+      [-g3(t) g*1(t), -g3(t) g*2(t), -|g3(t)|^2/2, 0, 0, ...],
+      ...
+      ]
+    and
+    G(t) = [g1(t), g2(t), ...]
+
+    Find master equation which results in the Heisenberg equations above:
+
+    rho'(t) = -i[H0(t), rho(t)] [unitary evolution]
+      -i[Hc(t), rho(t)]
+      -D_X(t)[rho(t)]
+
+    where
+    X(t) = g*1(t) A1(t) + g*2(t) A2(t) - ...
+    Hc(t) = -i sum_{i>j} (gi(t) g*j(t) Adi Aj - H.c.) / 2
+
+
     Args:
       dims (list): Dimensions of the individual systems.
       Hs (list): Hamiltonians of the individual systems
@@ -75,6 +75,9 @@ def solve_cascaded_system(dims, Hs, gs, *args, **kwargs):
           as functions of time.
       args (tuple): Additional arguments to be passed to qutip.mesolve.
       kwargs (dict): Additional arguments to be passed to qutip.mesolve.
+
+    Returns:
+      qutip.Result
     """
     A = [
         qt.tensor(*[
@@ -101,3 +104,44 @@ def solve_cascaded_system(dims, Hs, gs, *args, **kwargs):
 
     kwargs["c_ops"] = c_ops
     return qt.mesolve(H, *args, **kwargs)
+
+def get_emission_couplings(mode_shapes, t_range, Npts=1001):
+    """Calculate coupling to emit multiple modes in cascaded systems.
+
+    Args:
+      mode_shapes (list): Mode shapes as functions of time.
+      t_range (tuple): Initial and final time (t1, t2)
+      Npts (int, optional): Number of discretization points.
+
+    Returns:
+      list: List of functions of time, evaluating the couplings.
+    """
+
+    t_arr = np.linspace(*t_range, Npts)
+    dt = t_arr[1]-t_arr[0]
+    mode_shapes_samp = np.array([[f(t) for t in t_arr] for f in mode_shapes])
+
+    res = []
+    for i in range(len(mode_shapes_samp)):
+        mode = mode_shapes_samp[len(mode_shapes_samp)-i-1]
+        res.append(mode/np.sqrt(1-np.cumsum(abs(mode)**2)*dt))
+
+        fac = np.exp(np.cumsum(-abs(res[-1])**2/2)*dt)
+        for j in range(i+1, len(mode_shapes_samp)):
+            plt.plot(mode_shapes_samp[len(mode_shapes_samp)-j-1], "r-")
+            mode2 = mode_shapes_samp[len(mode_shapes_samp)-j-1]
+
+            A = np.cumsum(-res[-1]*mode2*fac)*dt
+            A -= A[-1]
+            A = A/fac
+            mode_shapes_samp[len(mode_shapes_samp)-j-1] = (
+                mode2-
+                res[-1].conjugate()*A
+                )
+            plt.plot(mode_shapes_samp[len(mode_shapes_samp)-j-1], "b-")
+            plt.show()
+
+    return [scipy.interpolate.interp1d(t_arr, pts,
+            bounds_error=False,
+            fill_value=0.0)
+        for pts in res[::-1]]
--- tests/test_kiilerich.py
+++ tests/test_kiilerich.py
@@ -3,6 +3,7 @@ import qutip as qt
 import matplotlib.pyplot as plt
 from quantum_optics.kiilerich import (
     solve_cascaded_system,
+    get_emission_couplings,
     )
 
 def test_cascaded_system1(plot=False):
@@ -54,7 +55,7 @@ def test_cascaded_system2(plot=False):
     System 1 = initially populated resonator with time-dependent coupling
         designed to release a 1/cosh(kappa*t/2) mode
     System 2 = initially empty resonator with time-dependent coupling
-        designed to catch the exponential mode from the spontaneous decay
+        designed to catch the emitted mode
 
     Passes if the total number of photons in the systems remains constant.
     """
@@ -145,8 +146,126 @@ def test_cascaded_system3(plot=False):
         plt.grid()
         plt.show()
 
+def test_emission_couplings1(plot=False):
+    """Test of emission couplings calculation with one mode.
+
+    System 1 = initially populated resonator with time-dependent coupling
+        designed to release a 1/cosh(kappa*t/2) mode. Coupling is calculated
+        automatically with the tested function here.
+    System 2 = initially empty resonator with time-dependent coupling
+        designed to catch the emitted mode.
+        Coupling is calculated manually.
+
+    Passes if the total number of photons in the systems remains constant.
+    """
+
+    dims = [5, 5]
+    Nph = 4
+    rho0 = qt.tensor(
+        qt.fock(dims[0], Nph)*qt.fock(dims[0], Nph).dag(),
+        qt.fock(dims[1], 0)*qt.fock(dims[1], 0).dag()
+        )
+    tlist = np.linspace(-10.0, 10.0, 1001)
+    Hs = [(lambda t, dim=dim: qt.qzero(dim)) for dim in dims]
+
+    kappa = 1.0
+    mode_shapes = [lambda t: np.sqrt(kappa) / (2*np.cosh(kappa*t/2))]
+    gs = get_emission_couplings(mode_shapes, (tlist[0], tlist[-1])) + [
+        lambda t: -0.5*np.sqrt(kappa) / np.cosh(kappa*t/2) / np.sqrt((1+np.tanh(kappa*t/2))/2)
+        ]
+
+    sol = solve_cascaded_system(dims, Hs, gs, rho0, tlist)
+
+    N1 = qt.tensor(
+        qt.destroy(dims[0]).dag()*qt.destroy(dims[0]),
+        qt.qeye(dims[1]))
+    N2 = qt.tensor(
+        qt.qeye(dims[0]),
+        qt.destroy(dims[1]).dag()*qt.destroy(dims[1]))
+    pops1 = np.array([qt.expect(N1, rho) for rho in sol.states])
+    pops2 = np.array([qt.expect(N2, rho) for rho in sol.states])
+
+    assert all(abs(pops1+pops2-Nph)<1e-3), "coupling calculation test failed"
+
+    if plot:
+        plt.plot(tlist, pops1+pops2-Nph)
+        plt.plot(tlist, pops1)
+        plt.plot(tlist, pops2)
+        plt.grid()
+        plt.show()
+
+
+def test_emission_couplings2(plot=False):
+    """Test of emission couplings calculation with two modes.
+
+    System 1 = initially populated resonator with time-dependent coupling
+        designed to release a 1/cosh(kappa*t/2) mode. Coupling is calculated
+        automatically with the tested function here.
+    System 2 = initially populated resonator with time-dependent coupling
+        designed to release a t/cosh(kappa*t/2) mode. Coupling is calculated
+        automatically with the tested function here.
+    System 3 = initially empty resonator with time-dependent coupling
+        designed to catch one of the emitted modes.
+        Coupling is calculated manually.
+
+    Passes if the total number of photons in the systems remains constant.
+    """
+
+    dims = [5, 5, 5]
+    Nph1 = 2
+    Nph2 = 1
+    rho0 = qt.tensor(
+        qt.fock(dims[0], Nph1)*qt.fock(dims[0], Nph1).dag(),
+        qt.fock(dims[1], Nph2)*qt.fock(dims[1], Nph2).dag(),
+        qt.fock(dims[2], 0)*qt.fock(dims[2], 0).dag()
+        )
+    tlist = np.linspace(-30.0, 30.0, 2001)
+    Hs = [(lambda t, dim=dim: qt.qzero(dim)) for dim in dims]
+
+    kappa = 1.0
+    mode_shapes = [
+        lambda t: np.sqrt(3*kappa**3)*t / (2*np.pi*np.cosh(kappa*t/2)),
+        lambda t: np.sqrt(kappa) / (2*np.cosh(kappa*t/2))
+        ]
+    gs_e = get_emission_couplings(mode_shapes, (tlist[0], tlist[-1]), Npts=40001)
+
+    gs = gs_e + [
+        lambda t: -0.5*np.sqrt(kappa) / np.cosh(kappa*t/2) / np.sqrt((1+np.tanh(kappa*t/2))/2)
+        ]
+
+    sol = solve_cascaded_system(dims, Hs, gs, rho0, tlist)
+
+    N1 = qt.tensor(
+        qt.destroy(dims[0]).dag()*qt.destroy(dims[0]),
+        qt.qeye(dims[1]),
+        qt.qeye(dims[2]))
+    N2 = qt.tensor(
+        qt.qeye(dims[0]),
+        qt.destroy(dims[1]).dag()*qt.destroy(dims[1]),
+        qt.qeye(dims[2]))
+    N3 = qt.tensor(
+        qt.qeye(dims[0]),
+        qt.qeye(dims[1]),
+        qt.destroy(dims[2]).dag()*qt.destroy(dims[2]))
+    pops1 = np.array([qt.expect(N1, rho) for rho in sol.states])
+    pops2 = np.array([qt.expect(N2, rho) for rho in sol.states])
+    pops3 = np.array([qt.expect(N3, rho) for rho in sol.states])
+
+    #assert all(abs(pops1+pops2-Nph)<1e-3), "coupling calculation test failed"
+
+    if plot:
+        plt.plot(tlist, pops2+pops3)
+        plt.plot(tlist, pops1)
+        plt.plot(tlist, pops2)
+        plt.plot(tlist, pops3)
+        plt.grid()
+        plt.show()
+
 
 if __name__ == "__main__":
-    test_cascaded_system1(plot=True)
-    test_cascaded_system2(plot=True)
-    test_cascaded_system3(plot=True)
+    # test_cascaded_system1(plot=True)
+    # test_cascaded_system2(plot=True)
+    # test_cascaded_system3(plot=True)
+
+    test_emission_couplings1(plot=True)
+    test_emission_couplings2(plot=True)
